2a56313f73ffcbcfa42cada6af3e1f812bd3d5c9
Antonio Gabriel <antonio.gab003@gmail.com>
2025-12-15 23:53:17 +0000

    15/12/2025- boanoite

--- src/pt/ulusofona/lp2/greatprogrammingjourney/Tools.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/Tools.java
@@ -5,12 +5,12 @@ public class Tools {
     int id;
     int posicao;
     boolean usada = false;
-    String nome;
+    String titulo;
 
     public Tools(int id, int posicao) {
         this.id = id;
         this.posicao = posicao;
-        this.nome = nome(id);
+        this.titulo = nome(id);
     }
 
     public boolean anula(int abismo) {
@@ -19,9 +19,39 @@ public class Tools {
 
     private String nome(int id) {
         String[] n = {
-                "Herança", "Programação Funcional", "Testes Unitários",
-                "Tratamento de Excepções", "IDE", "Ajuda Do Professor"
+                "Herança",
+                "Programação Funcional",
+                "Testes Unitários",
+                "Tratamento de Excepções",
+                "IDE",
+                "Ajuda Do Professor"
         };
         return n[id];
+    }
+
+    public static Tools[] guarda(String[][] data) {
+        if (data == null) {
+            return new Tools[0];
+        }
+
+        int c = 0;
+        for (String[] d : data) {
+            if (d[0].equals("1")) {
+                c++;
+            }
+        }
+
+        Tools[] r = new Tools[c];
+        int i = 0;
+
+        for (String[] d : data) {
+            if (d[0].equals("1")) {
+                r[i++] = new Tools(
+                        Integer.parseInt(d[1]),
+                        Integer.parseInt(d[2])
+                );
+            }
+        }
+        return r;
     }
 }
